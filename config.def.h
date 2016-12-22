24299fef9ffd9ebb4d6f87accb797171d033b0fb
Simon Bremer <simon.bremer@tum.de>
2016-12-22 19:46:24 +0100

    Applied clickonfocus patch;

--- config.def.h
+++ config.def.h
@@ -18,6 +18,9 @@ static const char *colors[][3]      = {
 	[SchemeSel] =  { col_gray4, col_cyan,  col_cyan  },
 };
 
+/* False means using the scroll wheel on a window will not change focus */
+static const Bool focusonwheelscroll = False;
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
